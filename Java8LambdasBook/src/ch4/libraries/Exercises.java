ab72e74e6463dbf6c3a0bcf3fe34b656cf164547
heeler <heeler@o2.pl>
2019-02-23 07:34:11 +0100

    some cahnges

---
+++ Java8LambdasBook/src/ch4/libraries/Exercises.java
@@ -0,0 +1,26 @@
+package ch4.libraries;
+
+import ch0.utilities.SampleData;
+
+public class Exercises {
+
+	public static void main(String[] args) {
+		
+		//start
+		excercise1();
+
+	}
+
+	
+	public static void excercise1()
+	{
+		
+		System.out.println("Exercise 1: count artists performing, in the case of groups, and any musicians who are members of those groups plus the group itself");
+		System.out.println("--------------------------------------------");
+		SampleData.albums
+			.forEach( album -> {  
+				System.out.println("the Album " + album.getName() + " has " + album.getAllMusicans().count() + " musicans");
+				});
+	}
+	
+}
